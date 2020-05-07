ccdd050f51b3af1a07628e86ab185c6459fe538c
Jignesh Chaudhary <44174121+J-Chaudhary@users.noreply.github.com>
2020-05-07 18:58:06 -0400

    first upload

---
+++ blog/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+def should_be_empty(value):
+    if value:
+        raise forms.ValidationError('Field is not empty...!')
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=80)
+    message = forms.CharField(widget=forms.Textarea)
+    email = forms.EmailField()
+    forcefield = forms.CharField(
+        required=False, widget=forms.HiddenInput,
+        label="Leave empty", validators=[should_be_empty])
+
+class UserCreateForm(UserCreationForm):
+    email = forms.EmailField(required=True, label='email', error_messages={'exists':'This already exists.'})
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(UserCreateForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+    def clean_emai(self):
+        if User.objects.filter(email=self.clean_data['email']).exists():
+            raise forms.ValidationError(self.fields['email'].error_message['exists'])
+        return self.clean_data['email']
